6ef27a1fea4e7270e3dd1c1372c5049f2a27d066
liangxinwei <liangxinwei@spacebox.fun>
2020-03-30 17:09:45 +0800

    update:start script

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,7 @@
+import logger from '../utils/logger';
+
 const env = process.env.NODE_ENV;
 export let isPro = env === 'production';
 export let isDev = env === 'development';
 export let isLocal = env === 'local' || env === 'local_dev';
+logger.info('env', env);
