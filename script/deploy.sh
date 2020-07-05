d4bb15b3dc6d8adcef93e3c022ff8de2b05bfe38
liangxinwei <lxw@seiue.com>
2020-07-05 23:25:39 +0800

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# --------------------------------------------------------------------
+# note:
+# this file should be execute by runing `npm run deolpy` in the project root dir,
+# or the `scp` command could not work as expected
+# --------------------------------------------------------------------
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 remoteIP="root@111.229.70.117"
 remotePath="/data/hotdog_backend"
@@ -10,23 +16,28 @@ printSuccessfulMessage() {
 
 echo -e "Deploying branch \033[33m$branch\033[0m."
 
-## step 0: check for uncommitted changes
-#git status
-#git diff-index --quiet HEAD -- || { echo '!! Stash or commit you changes first'; exit 1 }
+# step 0: check for uncommitted changes
+git status
+if [[ !(-z $(git status -s)) ]];
+then
+  echo -e "\033[31m !! Stash or commit your changes first. \033[0m"
+  exit 1
+fi
 
-## step 1: push current branch to remote
-#echo '\n-- Pushing branch to remote ...\n'
-#git push || exit 1
+# step 1: push current branch to remote
+echo 'Pushing branch to remote ...'
+git push || exit 1
 
 # step 2: upload assets
 npm run build
 cp -r src/public dist
 printSuccessfulMessage "copy dir src/public to dist"
-scp "/e/works/my/hotdog-backend/package.json" "$remoteIP:$remotePath"
+scp ./package.json "$remoteIP:$remotePath" || exit 1
 printSuccessfulMessage "upload file package.json"
-scp -r "/e/works/my/hotdog-backend/dist/" "$remoteIP:$remotePath"
+scp -r ./dist/ "$remoteIP:$remotePath" || exit 1
 printSuccessfulMessage "upload dir dist"
-scp "/e/works/my/hotdog-backend/script/production.js" "$remoteIP:$remotePath/dist/config"
+#[[ ! "test1.sh" ]] || { echo "123"; exit 1; }
+scp ./script/production.js "$remoteIP:$remotePath/dist/config" || exit 1
 printSuccessfulMessage "upload file production.js"
 
 # step 3: update node_modules
@@ -34,4 +45,4 @@ echo 'Installing node_modules ...'
 yarn install || exit 1
 
 # step 4: start serve
-#ssh $remoteIP "nohup $remotePath/start.sh"
+ssh $remoteIP "$remotePath/start.sh"
