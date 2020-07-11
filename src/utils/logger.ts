1ef28970b129aa0d47fb1cfab9c5032cb8abbfcc
liangxinwei <sivan.shanghai@gmail.com>
2020-07-11 12:06:51 +0800

    update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,50 +1,6 @@
 import * as winston from 'winston';
 import DayJS from 'dayjs';
 
-class Logger {
-  private readonly consoleLevel: string;
-  private readonly fileLevel: string;
-
-  constructor() {
-    // this.consoleLevel = logConfig.consoleLevel;
-    // this.fileLevel = logConfig.fileLevel;
-    this.consoleLevel = 'debug';
-    this.fileLevel = 'info';
-
-    this.addLogger('controller');
-    this.addLogger('service');
-  }
-
-  public getLogger(category: string): winston.Logger {
-    return winston.loggers.get(category);
-  }
-
-  private addLogger(category: string) {
-    winston.loggers.add(category, {
-      format: winston.format.combine(
-        winston.format.label({ label: category}),
-        winston.format.timestamp(),
-        winston.format.printf((info) => {
-          return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
-        })
-      ),
-
-      transports: [
-        // level设置打印的最低级别
-        new winston.transports.Console({ level: this.consoleLevel }),
-        new winston.transports.File({
-          level: this.fileLevel,
-          filename: `logs/${category}.log`,
-          maxsize: 5242880,
-          maxFiles: 20
-        })
-      ]
-    });
-  }
-}
-
-// const LogFactory = new Logger();
-
 const options: winston.LoggerOptions = {
   format: winston.format.combine(
     winston.format.label({label: 'default'}),
